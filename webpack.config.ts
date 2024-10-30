171d35d881ef7f547669e7a1df90082cdb607ed6
walnuts1018 <r.juglans.1018@gmail.com>
2024-10-31 01:59:14 +0900

    fix webpack config
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,34 +1,32 @@
-import { ConfigurationFactory } from 'webpack'
 import path from 'path'
 import CopyWebpackPlugin from 'copy-webpack-plugin'
+import { Configuration } from "webpack";
 
-const webpackConfig: ConfigurationFactory = () => {
-  return {
-    entry: {
-      main: path.join(__dirname, 'src', 'main.ts'),
-    },
-    output: {
-      path: path.join(__dirname, 'dist'),
-      filename: '[name].js',
-    },
-    module: {
-      rules: [
-        {
-          test: /.tsx?$/,
-          use: 'ts-loader',
-          exclude: '/node_modules/',
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.json'],
-    },
-    plugins: [
-      new CopyWebpackPlugin({
-        patterns: [{ from: 'public', to: '.' }],
-      }),
+const webpackConfig: Configuration = {
+  entry: {
+    main: path.join(__dirname, 'src', 'main.ts'),
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/',
+      },
     ],
-  }
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'public', to: '.' }],
+    }),
+  ],
 }
 
 export default webpackConfig
